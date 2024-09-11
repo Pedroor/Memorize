c941922dc6c75605ff04652cd92891ac9bf08f42
Pedro Andrade <parthur259@gmail.com>
2024-09-11 09:05:04 -0300

    feature: lesson 1 swift ui concepts

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Pedro Andrade on 10/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
